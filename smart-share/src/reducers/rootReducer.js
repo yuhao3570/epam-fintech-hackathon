298fa9256c0270359077d4e458bcd735174c96e9
Honda Hong <Honda_Hong@epam.com>
2019-12-07 18:20:01 +0800

    add reducers into root

--- smart-share/src/reducers/rootReducer.js
+++ smart-share/src/reducers/rootReducer.js
@@ -1,7 +1,19 @@
 import { combineReducers } from 'redux';
 
+import peopleCount from 'reducers/peopleCount';
+import items from 'reducers/items';
+import users from 'reducers/users';
+
+function setReducer(state, action, pairs) {
+  if (action.type in pairs) return pairs[action.type](state, action);
+  return state;
+}
+
+
 const rootReducer = combineReducers({
-  // ...
+  peopleCount: (state = [], action) => setReducer(state, action, peopleCount),
+  items: (state = [], action) => setReducer(state, action, items),
+  users: (state = [], action) => setReducer(state, action, users),
 });
 
 export default rootReducer;
