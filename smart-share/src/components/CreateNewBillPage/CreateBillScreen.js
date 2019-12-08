d30797ee0d0898f3ff63768f0b9bcaeac69adf03
Hao <yuhao3570@gmail.com>
2019-12-08 11:05:03 +0800

    create new bill done, need styling

--- smart-share/src/components/CreateNewBillPage/CreateBillScreen.js
+++ smart-share/src/components/CreateNewBillPage/CreateBillScreen.js
@@ -1,23 +1,63 @@
-import React from 'react';
-import { View, Text, Input, Button} from 'native-base';
+import React, {useState} from 'react';
+import { View, Text, Input, Button, Content, Container} from 'native-base';
 import styles from 'styles/CreateBillScreenStyle';
+import { connect } from 'react-redux';
 
-export default function CreateBillScreen({ navigation }){
+function CreateBillScreen({ navigation, updatePeopleCount, updateItems, itemInfo }){
+  const [numberOfPeople, setNumberOfPeopel] = useState('');
+  const [inputedItems, setInputItems] = useState(itemInfo);
+
+  const handlePeopelChange = ({nativeEvent}) => {
+    setNumberOfPeopel(nativeEvent.text);
+  }
+  
+  const resetItemKey = (event, key) => {
+    const tempItems = inputedItems;
+    tempItems[event.nativeEvent.text] = tempItems[key];
+    delete tempItems[key]; 
+    setInputItems(tempItems);
+  }
+
+  const navToNextPage = () => {
+    updatePeopleCount(parseInt(numberOfPeople));
+    updateItems(inputedItems);
+    navigation.navigate('SelectPage');
+  }
+
   return (
-    <View style={styles.createBillView}>
-      <Text >Add later</Text>
-      <Input></Input>
-      <Button
-        onPress={()=>{navigation.navigate('SelectPage')}}><Text>走</Text></Button>
-    </View>
-   
-  )
+    <Container style={styles.createBillContainer}>
+      <View style={styles.peopleView}>
+        <Text >Enter the number of people to share:</Text>
+        <Input
+        placeholder='hello'
+          value={numberOfPeople}
+          onChange={handlePeopelChange}
+        />
+      </View>
+
+      <Text >Items</Text>
+      <Content style={styles.itemContent}>
+        {Object.keys(inputedItems).map(key => 
+          <Input
+            key={key}
+            value={key}
+            onChange={(event) => resetItemKey(event, key)}
+            style={styles.itemInput}
+          />
+        )}
+      </Content>
+      <Button onPress={navToNextPage}><Text>走</Text></Button>
+    </Container>
+  );
 }
 
 const mapStateToProps = ({items}) => ({
-  // items.;
+  itemInfo: items,
 })
 
 const mapDispatchToProps = (dispatch) => ({
-
+  updatePeopleCount: (number) => {dispatch({type: 'SET_PEOPLE_COUNT', payload: {number}})},
+  updateItems: (items) => {dispatch({type: 'SET_ITEMS', payload: {items}})}
 })
+
+export default connect(mapStateToProps, mapDispatchToProps)(CreateBillScreen)
--- smart-share/src/styles/CreateBillScreenStyle.js
+++ smart-share/src/styles/CreateBillScreenStyle.js
@@ -1,10 +1,22 @@
 import { StyleSheet } from 'react-native';
 
 export default StyleSheet.create({
-  createBillView: {
-    marginTop: 100,
+  createBillContainer: {
+    height: 300,
+  },
+  peopleView: {
+    marginLeft: 10,
+    height: '10%',
+    justifyContent: 'space-around',
+  },
+  peopleInput: {
     justifyContent: 'space-around',
     alignItems: 'center'
   },
-  
+  itemContent: {
+  },
+  itemInput: {
+    justifyContent: 'space-around',
+    alignItems: 'center'
+  }
 })
